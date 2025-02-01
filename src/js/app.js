aac1e5cf73f4c55da310a906975c6b1ed28cf413
ArifMiah07 <arifmiah.me322@gmail.com>
2025-02-01 15:39:11 +0600

    init

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+console.log('hello world');
+
+const getStart = document.getElementById('gets');
+const joinNow = document.getElementById('joinn');
+
+getStart.addEventListener('click', function(){
+    alert('hey bro, u have clicked on the get start button!')
+    console.log('u have been clicked on the get start button!')
+});
+joinNow.addEventListener('click', function(){
+    alert('hey bro, u have clicked on the join now button!')
+    console.log('u have been clicked on the join now button!')
+});
+
